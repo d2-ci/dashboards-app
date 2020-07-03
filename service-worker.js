ef8bc7e7218acca88078245e4e0adfb0ab6d1847
GitHub <noreply@github.com>
2020-07-03 08:04:04 +0000

    c083efd chore(deps-dev): bump eslint-plugin-react-hooks from 2.5.1 to 4.0.5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.18fd408593927d2c06c7c25f440f5ea0.js"
+  "./precache-manifest.e944aa1fc4c90f291ed5f116881c6792.js"
 );
 
 workbox.clientsClaim();
