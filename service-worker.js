43b2b6c4cbb8f74e5980d8919af0e6c146ed19f8
GitHub <noreply@github.com>
2020-08-03 06:56:02 +0000

    643a36b chore(deps): bump @dhis2/analytics from 7.1.5 to 8.0.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7cad5e95cad00b3e0d28e912a49765c3.js"
+  "./precache-manifest.1648c54b30f4b02845c1e8fd7bcfbc32.js"
 );
 
 workbox.clientsClaim();
