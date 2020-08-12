966379335ab9d28f426c1132d3578449d43ef9a3
GitHub <noreply@github.com>
2020-08-12 08:13:08 +0000

    f78a8a3 chore(deps): bump react-grid-layout from 0.18.3 to 1.0.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bb4ee6f77438c36a16a65d101068e821.js"
+  "./precache-manifest.47322445465b558239db74e3da5e136a.js"
 );
 
 workbox.clientsClaim();
