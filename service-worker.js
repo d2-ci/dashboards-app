09d7d02ba10fb4f6787c218b232849a39046ac3c
GitHub <noreply@github.com>
2020-09-04 13:53:59 +0000

    ed049fe fix: apply scrollbar to the dashboard area below the control bars and headerbar [DHIS2-9371] (#1034)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d0a1981717337a0ef5d704b16da7754b.js"
+  "./precache-manifest.a47335f6602c1e3bb835081f7ab3effa.js"
 );
 
 self.addEventListener('message', (event) => {
