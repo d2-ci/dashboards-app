6455a42d368d4d27b3759e7d91b1a1d05404a728
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-09-08 16:21:18 +0000

    c9d9a8b chore: update deps

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f0fbeb01b51e20118bcd5ddc52e4e3c.js"
+  "./precache-manifest.5e3fc10515e5fd10d75f15f160637352.js"
 );
 
 self.addEventListener('message', (event) => {
