709e47256a52ad39122bebecb8a8587edd994e01
GitHub <noreply@github.com>
2020-09-21 17:26:52 +0000

    c5f6518 chore(deps): bump moment from 2.27.0 to 2.28.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.234b0be3026968e6b16ef4c940557a02.js"
+  "./precache-manifest.032954a92685abd98d6656584282ea89.js"
 );
 
 self.addEventListener('message', (event) => {
