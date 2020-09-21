a26d8585af46fa09605cbe6c23ad4595be499449
GitHub <noreply@github.com>
2020-09-21 16:59:54 +0000

    1215165 chore(deps): bump whatwg-fetch from 3.4.0 to 3.4.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.069b4a2f2deebb025665421063fcf856.js"
+  "./precache-manifest.499d7928dfb47e4a22fe345ce40bf46c.js"
 );
 
 self.addEventListener('message', (event) => {
