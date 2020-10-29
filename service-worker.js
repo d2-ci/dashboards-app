ccb8dba11cd04db714d08bd20f8ddc55b7533214
GitHub <noreply@github.com>
2020-10-29 14:03:20 +0000

    b69e690 Merge branch 'master' into test/both-view-and-edit-cypress-tests

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.99fac755be614d1cdf661bb1ba361958.js"
+  "./precache-manifest.0bb5de30708901e7de1f4fb8fdcb7a82.js"
 );
 
 self.addEventListener('message', (event) => {
