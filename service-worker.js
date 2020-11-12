270e0d8dba025f933c4791b132183090472bbd27
GitHub <noreply@github.com>
2020-11-12 08:44:37 +0000

    d1df2b3 chore(deps): bump typeface-roboto from 0.0.75 to 1.1.13

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9c1dd1b49b7d0e54ec17f2d2b63cb534.js"
+  "./precache-manifest.1b642d1084aabae14209cf587fbde9f4.js"
 );
 
 self.addEventListener('message', (event) => {
